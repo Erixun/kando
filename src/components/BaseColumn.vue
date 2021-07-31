72b7cd5ab132d4f58accbe1a98ecbc8159cd2a93
Erik Sundberg <erik.sundberg@unicus.com>
2021-07-31 18:00:45 +0200

    create BaseColumn

---
+++ src/components/BaseColumn.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="column" id="backlog">{{headerText}}</article>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "BaseColumn",
+  props: {
+    // data: {
+    //   type: Object,
+    //   required: true,
+    // },
+    title: {
+      type: String,
+      required: true,
+    }
+  },
+  setup(props) {
+    
+    return {headerText: props.title}
+  },
+})
+</script>
