eabbd9283afda58e66c323a9ace5f67cea09adde
Erik Sundberg <erik.sundberg@unicus.com>
2021-07-31 15:52:34 +0200

    create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/board">Board</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "TheHeader",
+  // setup() {
+    
+  // },
+})
+</script>
