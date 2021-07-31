eb1505bfd749e9c06563a7f438fb9d83023faae8
Erik Sundberg <erik.sundberg@unicus.com>
2021-07-31 15:52:49 +0200

    create board component

---
+++ src/views/KanbanBoard.vue
@@ -0,0 +1,18 @@
+<template>I am Board</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "KanbanBoard",
+  props: {
+    data: {
+      type: String,
+      required: false,
+    }
+  },
+  setup(props) {
+    return props;
+  },
+});
+</script>
