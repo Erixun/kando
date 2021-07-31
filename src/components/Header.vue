9799d6ae17bc0efe096a2fe8d693b235febd1b7c
Erik Sundberg <erik.sundberg@unicus.com>
2021-07-31 17:18:52 +0200

    header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,32 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/board">Board</router-link>
-  </div>
+  <header>
+    <div id="header-logo">
+      <h1>KanDo!</h1>
+    </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/board">Board</router-link>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "TheHeader",
   // setup() {
-    
+
   // },
-})
+});
 </script>
+
+<style lang="scss" scoped>
+header {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  grid-gap: 10vw;
+  padding-left: 0.5rem;
+}
+</style>
