23b520ca8fce3ae5c7ec695d278a3ff0e7f292c0
Erik Sundberg <erik.sundberg@unicus.com>
2021-08-01 22:08:34 +0200

    add dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import "reflect-metadata"
 
 createApp(App).use(router).mount("#app");
