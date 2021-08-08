7f3ed982a848c3a3ad64a3ef33bab5682e199df5
Erik Sundberg <erik.sundberg@unicus.com>
2021-08-08 21:38:28 +0200

    add task method to easily get a task

--- src/store/TaskList.ts
+++ src/store/TaskList.ts
@@ -6,6 +6,11 @@ import ITaskList from "./interface/ITaskList";
 export default class TaskList implements ITaskList {
   tasks: ITask[] = [];
 
+  task(id: string): ITask | undefined {
+    const found = this.tasks.find(t => t.task_id == id)
+    return found
+  }
+
   insert(task: ITask, pos: number): void {
     this.tasks.splice(pos, 0, task);
   }
