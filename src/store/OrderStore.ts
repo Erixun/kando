dc6051728674a861060bd8463658d7be00f0ada6
Erik Sundberg <erik.sundberg@unicus.com>
2021-08-08 21:34:43 +0200

    create OrderStore

---
+++ src/store/OrderStore.ts
@@ -0,0 +1,19 @@
+import { singleton } from "tsyringe";
+import { ColKey } from "./constant/ColKey";
+import IOrderStore from "./interface/IOrderStore";
+import ColString from "./types/ColString";
+
+@singleton()
+export default class OrderStore implements Record<ColString, string[]>, IOrderStore {
+  [ColKey.Backlog]: string[];
+  [ColKey.UpNext]: string[];
+  [ColKey.Doing]: string[];
+  [ColKey.Done]: string[];
+
+  constructor() {
+    this.backlog = [];
+    this.upnext = [];
+    this.doing = [];
+    this.done = []
+  }
+}
