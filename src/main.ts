6001fb3b210940ddc79addf677c10f99e9b7b316
Erik Sundberg <erik.sundberg@unicus.com>
2021-08-21 19:48:26 +0200

    add fontawesome

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlus, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).use(router).mount("#app");
+library.add( faPlus, faExpandAlt );
+
+createApp(App).use(router).component("fa-icon", FontAwesomeIcon).mount("#app");
