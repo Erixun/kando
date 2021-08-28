334d97839a2c95d90fca03e271548056d8cd8d33
Erik Sundberg <erik.sundberg@unicus.com>
2021-08-28 17:36:59 +0200

    new check icon

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faExpandAlt, faCompressAlt, faInbox, faCheckSquare } from "@fortawesome/free-solid-svg-icons";
+import { faExpandAlt, faCompressAlt, faInbox, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faExpandAlt, faCompressAlt, faInbox, faCheckSquare);
+library.add(faExpandAlt, faCompressAlt, faInbox, faCheck);
 
 createApp(App).use(router).component("fa-icon", FontAwesomeIcon).mount("#app");
