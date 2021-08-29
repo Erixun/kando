195d39ecb3753ffeb3a3953b65d04b00f7e69c60
Erik Sundberg <erik.sundberg@unicus.com>
2021-08-29 10:11:54 +0200

    lint fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faExpandAlt, faCompressAlt, faInbox, faCheck } from "@fortawesome/free-solid-svg-icons";
+import {
+  faExpandAlt,
+  faCompressAlt,
+  faInbox,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faExpandAlt, faCompressAlt, faInbox, faCheck);
