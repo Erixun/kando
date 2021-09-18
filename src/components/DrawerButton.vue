42747329b59744cf431af53786245c6f8f9e6be9
Erik Sundberg <erik.sundberg@unicus.com>
2021-09-18 14:36:09 +0200

    create drawer-button component

---
+++ src/components/DrawerButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button :id="drawerBtnId" class="drawer-btn" v-touch:release="toggleClosed">
+    <fa-icon :id="compressId" icon="compress-alt"></fa-icon>
+    <fa-icon :id="expandId" icon="expand-alt"></fa-icon>
+  </button>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "DrawerButton",
+  props: {
+    drawerId: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    toggleClosed() {
+      const drawer = document.getElementById(this.targetId);
+      drawer
+        ? drawer.classList.toggle("closed")
+        : () => {
+            throw Error("element with id drawer not found");
+          };
+
+      if (window.innerWidth < 580) {
+        //hide other drawer when one is open
+        const drawers = document.getElementsByClassName("drawer");
+        if (!drawer?.classList.contains("closed")) {
+          const closed = Array.from(drawers).find((d) =>
+            d.classList.contains("closed")
+          );
+          closed?.classList.add("hidden");
+        } else {
+          const hidden = Array.from(drawers).find((d) =>
+            d.classList.contains("hidden")
+          );
+          hidden?.classList.remove("hidden");
+        }
+      }
+    },
+  },
+  setup(props) {
+    return {
+      targetId: props.drawerId,
+      drawerBtnId: computed(() => `${props.drawerId}-drawer-btn`),
+      compressId: computed(() => `${props.drawerId}-compress`),
+      expandId: computed(() => `${props.drawerId}-expand`),
+    };
+  },
+});
+</script>
